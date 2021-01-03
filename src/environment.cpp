f466286bf69a897184e88e20d5e1956640747452
Elias Tragas <Trags@Kathys-MacBook-Pro.local>
2021-01-03 15:20:26 -0800

    stop using c++17 features

--- src/environment.cpp
+++ src/environment.cpp
@@ -8,13 +8,13 @@
 // using templates for processPointClouds so also include .cpp to help linker
 #include "processPointClouds.cpp"
 
-std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr& viewer)
+std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer::Ptr &viewer)
 {
 
-    Car egoCar( Vect3(0,0,0), Vect3(4,2,2), Color(0,1,0), "egoCar");
-    Car car1( Vect3(15,0,0), Vect3(4,2,2), Color(0,0,1), "car1");
-    Car car2( Vect3(8,-4,0), Vect3(4,2,2), Color(0,0,1), "car2");	
-    Car car3( Vect3(-12,4,0), Vect3(4,2,2), Color(0,0,1), "car3");
+    Car egoCar(Vect3(0, 0, 0), Vect3(4, 2, 2), Color(0, 1, 0), "egoCar");
+    Car car1(Vect3(15, 0, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car1");
+    Car car2(Vect3(8, -4, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car2");
+    Car car3(Vect3(-12, 4, 0), Vect3(4, 2, 2), Color(0, 0, 1), "car3");
 
     std::vector<Car> cars;
     cars.push_back(egoCar);
@@ -22,7 +22,7 @@ std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer
     cars.push_back(car2);
     cars.push_back(car3);
 
-    if(renderScene)
+    if (renderScene)
     {
         renderHighway(viewer);
         egoCar.render(viewer);
@@ -34,13 +34,12 @@ std::vector<Car> initHighway(bool renderScene, pcl::visualization::PCLVisualizer
     return cars;
 }
 
-
-void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
+void simpleHighway(pcl::visualization::PCLVisualizer::Ptr &viewer)
 {
     // ----------------------------------------------------
     // -----Open 3D viewer and display simple highway -----
     // ----------------------------------------------------
-    
+
     // RENDER OPTIONS
     bool renderScene = true;
     std::vector<Car> cars = initHighway(renderScene, viewer);
@@ -50,112 +49,115 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // renderRays(viewer, lidar->position, pointCloud);
     // renderPointCloud(viewer, pointCloud, std::string("name"), Color(0, 1, 0));
     ProcessPointClouds<pcl::PointXYZ> cloudProcess;
-    auto [planeCloud, segmentCloud] = cloudProcess.SegmentPlane(pointCloud, 100, 0.2);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr planeCloud;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr segmentCloud;
+    std::tie(planeCloud, segmentCloud) = cloudProcess.SegmentPlane(pointCloud, 100, 0.2);
     // renderPointCloud(viewer, planeCloud, std::string("plane"), Color(0, 1, 0));
     // renderPointCloud(viewer, segmentCloud, std::string("obstacle"), Color(1, 0, 0));
-    // TODO:: Create lidar sensor 
-
+    // TODO:: Create lidar sensor
 
     // TODO:: Create point processor
     auto pointProcessor = ProcessPointClouds<pcl::PointXYZ>();
     std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloudClusters = pointProcessor.Clustering(segmentCloud, 5, 3, 30);
 
-
     int clusterId = 0;
-    std::vector<Color> colors = {Color(1,0,0), Color(0,1,0), Color(0,0,1)};
+    std::vector<Color> colors = {Color(1, 0, 0), Color(0, 1, 0), Color(0, 0, 1)};
 
-    for(pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
+    for (pcl::PointCloud<pcl::PointXYZ>::Ptr cluster : cloudClusters)
     {
         pointProcessor.numPoints(cluster);
-        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId]);
+        renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId]);
         // Box box = pointProcessor.BoundingBox(cluster);
         // renderBox(viewer,box,clusterId);
         ++clusterId;
     }
 }
 
-void cityBlock(pcl::visualization::PCLVisualizer::Ptr& viewer, ProcessPointClouds<pcl::PointXYZI>* pointProcessor, const pcl::PointCloud<pcl::PointXYZI>::Ptr& inputCloud)
+void cityBlock(pcl::visualization::PCLVisualizer::Ptr &viewer, ProcessPointClouds<pcl::PointXYZI> *pointProcessor, const pcl::PointCloud<pcl::PointXYZI>::Ptr &inputCloud)
 {
-  // ----------------------------------------------------
-  // -----Open 3D viewer and display City Block     -----
-  // ----------------------------------------------------
+    // ----------------------------------------------------
+    // -----Open 3D viewer and display City Block     -----
+    // ----------------------------------------------------
 
-  // Experiment with the ? values and find what works best
-  // What does filterres do?
-  pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessor->FilterCloud(inputCloud, .2 , Eigen::Vector4f (-25, -6, -10, 1), Eigen::Vector4f (25, 6, 10, 1));
-        std::cout << "cluster size ";
-  auto [planeCloud, segmentCloud] = pointProcessor->SegmentPlane(filterCloud, 100, -1.9);
-  renderPointCloud(viewer, planeCloud, std::string("plane"), Color(0, 1, 0));
-  std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessor->Clustering(segmentCloud, 2, 20, 500);
-  int clusterId = 0;
-  std::vector<Color> colors = {Color(1,0,0), Color(1, 1,0), Color(0,0 , 1), Color(1, 1, 1), Color(0, 1, 1), Color(1,0,1)};
-  for(pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters)
+    // Experiment with the ? values and find what works best
+    // What does filterres do?
+    pcl::PointCloud<pcl::PointXYZI>::Ptr filterCloud = pointProcessor->FilterCloud(inputCloud, .1, Eigen::Vector4f(-25, -6, -10, 1), Eigen::Vector4f(25, 6, 10, 1));
+    std::cout << "cluster size ";
+    pcl::PointCloud<pcl::PointXYZI>::Ptr planeCloud;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr segmentCloud;
+    std::tie(planeCloud, segmentCloud) = pointProcessor->SegmentPlane(filterCloud, 100, -1.9);
+    renderPointCloud(viewer, planeCloud, std::string("plane"), Color(0, 1, 0));
+    std::vector<pcl::PointCloud<pcl::PointXYZI>::Ptr> cloudClusters = pointProcessor->Clustering(segmentCloud, 2, 20, 500);
+    int clusterId = 0;
+    std::vector<Color> colors = {Color(1, 0, 0), Color(1, 1, 0), Color(0, 0, 1), Color(1, 1, 1), Color(0, 1, 1), Color(1, 0, 1)};
+    for (pcl::PointCloud<pcl::PointXYZI>::Ptr cluster : cloudClusters)
     {
         pointProcessor->numPoints(cluster);
-        renderPointCloud(viewer,cluster,"obstCloud"+std::to_string(clusterId),colors[clusterId%6]);
+        renderPointCloud(viewer, cluster, "obstCloud" + std::to_string(clusterId), colors[clusterId % 6]);
         Box box = pointProcessor->BoundingBox(cluster);
-        renderBox(viewer,box,clusterId);
+        renderBox(viewer, box, clusterId);
         ++clusterId;
     }
 }
 
-
-
 //setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
-void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& viewer)
+void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr &viewer)
 {
 
-    viewer->setBackgroundColor (0, 0, 0);
-    
+    viewer->setBackgroundColor(0, 0, 0);
+
     // set camera position and angle
     viewer->initCameraParameters();
     // distance away in meters
     int distance = 16;
-    
-    switch(setAngle)
+
+    switch (setAngle)
     {
-        case XY : viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0); break;
-        case TopDown : viewer->setCameraPosition(0, 0, distance, 1, 0, 1); break;
-        case Side : viewer->setCameraPosition(0, -distance, 0, 0, 0, 1); break;
-        case FPS : viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
+    case XY:
+        viewer->setCameraPosition(-distance, -distance, distance, 1, 1, 0);
+        break;
+    case TopDown:
+        viewer->setCameraPosition(0, 0, distance, 1, 0, 1);
+        break;
+    case Side:
+        viewer->setCameraPosition(0, -distance, 0, 0, 0, 1);
+        break;
+    case FPS:
+        viewer->setCameraPosition(-10, 0, 0, 0, 0, 1);
     }
 
-    if(setAngle!=FPS)
-        viewer->addCoordinateSystem (1.0);
+    if (setAngle != FPS)
+        viewer->addCoordinateSystem(1.0);
 }
 
-
-int main (int argc, char** argv)
+int main(int argc, char **argv)
 {
     std::cout << "starting enviroment" << std::endl;
 
-
-
-
-    pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+    pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
-    ProcessPointClouds<pcl::PointXYZI>* pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
-    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_1");
+    ProcessPointClouds<pcl::PointXYZI> *pointProcessorI = new ProcessPointClouds<pcl::PointXYZI>();
+    std::vector<boost::filesystem::path> stream = pointProcessorI->streamPcd("../src/sensors/data/pcd/data_2");
     auto streamIterator = stream.begin();
     pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloudI;
 
     // cityBlock(viewer, pointProcessorI, inputCloudI);
-    while (!viewer->wasStopped ())
+    while (!viewer->wasStopped())
     {
 
-    // Clear viewer
-    viewer->removeAllPointClouds();
-    viewer->removeAllShapes();
+        // Clear viewer
+        viewer->removeAllPointClouds();
+        viewer->removeAllShapes();
 
-    // Load pcd and run obstacle detection process
-    inputCloudI = pointProcessorI->loadPcd((*streamIterator).string());
-    cityBlock(viewer, pointProcessorI, inputCloudI);
+        // Load pcd and run obstacle detection process
+        inputCloudI = pointProcessorI->loadPcd((*streamIterator).string());
+        cityBlock(viewer, pointProcessorI, inputCloudI);
 
-    streamIterator++;
-    if(streamIterator == stream.end())
-        streamIterator = stream.begin();
+        streamIterator++;
+        if (streamIterator == stream.end())
+            streamIterator = stream.begin();
 
-    viewer->spinOnce ();
+        viewer->spinOnce();
     }
 }
--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -5,7 +5,7 @@
 #include <pcl/common/distances.h>
 
 // Structure to represent node of kd tree
-template<typename PointT>
+template <typename PointT>
 struct Node
 {
 	PointT point;
@@ -14,64 +14,74 @@ struct Node
 	std::shared_ptr<Node> right;
 
 	Node(PointT arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+		: point(arr), id(setId), left(NULL), right(NULL)
+	{
+	}
 };
 
-template<typename PointT>
+template <typename PointT>
 struct KdTree
 {
 	std::shared_ptr<Node<PointT>> root;
 
 	KdTree()
-	: root(NULL)
-	{}
+		: root(NULL)
+	{
+	}
 
 	void insert(PointT point, int id)
 	{
 		// Choice is to go left, right or insert
 		// Want to keep track of depth
-		if (root == NULL){
+		if (root == NULL)
+		{
 			root = std::make_shared<Node<PointT>>(point, id);
 			return;
 		}
 		int depth = 0;
 		std::shared_ptr<Node<PointT>> cur_node = root;
-		while (1) {
-			float cur_node_val = cur_node->point.data[depth%3];
-			float insert_val = point.data[depth%3];
-			if (cur_node_val < insert_val){
+		while (1)
+		{
+			float cur_node_val = cur_node->point.data[depth % 3];
+			float insert_val = point.data[depth % 3];
+			if (cur_node_val < insert_val)
+			{
 				// Go right
-				if (cur_node->right == NULL) {
+				if (cur_node->right == NULL)
+				{
 					cur_node->right = std::make_shared<Node<PointT>>(point, id);
 					return;
 				}
 				cur_node = cur_node->right;
 			}
-			else {
-				if (cur_node->left == NULL) {
+			else
+			{
+				if (cur_node->left == NULL)
+				{
 					cur_node->left = std::make_shared<Node<PointT>>(point, id);
 					return;
 				}
 				cur_node = cur_node->left;
-
 			}
 			depth += 1;
 		}
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
-
+		// the function should create a new node and place correctly with in the root
 	}
 
-	bool nodeInBox(PointT target, float distanceTol, std::shared_ptr<Node<PointT>> node){
-		for(int i = 0; i < 3; ++i) {
-			if (!(std::abs(target.data[i] - node->point.data[i]) < distanceTol)) return false;
+	bool nodeInBox(PointT target, float distanceTol, std::shared_ptr<Node<PointT>> node)
+	{
+		for (int i = 0; i < 3; ++i)
+		{
+			if (!(std::abs(target.data[i] - node->point.data[i]) < distanceTol))
+				return false;
 		}
 		return true;
 	}
 
-	bool nodeInCircle(PointT target, float distanceTol, std::shared_ptr<Node<PointT>> node){
-		return pcl::euclideanDistance(target,node->point) < distanceTol;
+	bool nodeInCircle(PointT target, float distanceTol, std::shared_ptr<Node<PointT>> node)
+	{
+		return pcl::euclideanDistance(target, node->point) < distanceTol;
 		// float distance = 0;
 		// for(std::size_t i = 0; i < 3; ++i) {
 		// 	distance += std::pow(target.data[i] - node->point.data[i], 2);
@@ -88,14 +98,20 @@ struct KdTree
 		// If a point is in the box, check if in the circle, then add to ids
 		// If a point is in the box, then both of its partitions are candidates for inclusion
 		std::stack<std::tuple<std::shared_ptr<Node<PointT>>, int>> frontier;
+		int depth;
+		std::shared_ptr<Node<PointT>> current_node;
 		frontier.push(std::make_tuple(this->root, 0));
-		while (!frontier.empty()) {
-			auto [current_node, depth] = frontier.top();
+		while (!frontier.empty())
+		{
+			std::tie(current_node, depth) = frontier.top();
 			frontier.pop();
-			if (current_node == NULL) continue;
+			if (current_node == NULL)
+				continue;
 
-			if (nodeInBox(target, distanceTol, current_node)) {
-				if (nodeInCircle(target, distanceTol, current_node)){
+			if (nodeInBox(target, distanceTol, current_node))
+			{
+				if (nodeInCircle(target, distanceTol, current_node))
+				{
 					// std::cout << "WE have a match with id " << current_node->id << std::endl;
 					ids.push_back(current_node->id);
 				}
@@ -103,25 +119,27 @@ struct KdTree
 			// My code assumed that being in the box was equiv to being on either side
 			// But not true because could fail box test in some dimension but not in comparison one
 			// Check if splits on left side
-			if (std::abs(current_node->point.data[depth%3] - target.data[depth%3]) < distanceTol) {
-				frontier.push(std::make_tuple(current_node->left, depth+1));
-				frontier.push(std::make_tuple(current_node->right, depth+1));
+			if (std::abs(current_node->point.data[depth % 3] - target.data[depth % 3]) < distanceTol)
+			{
+				frontier.push(std::make_tuple(current_node->left, depth + 1));
+				frontier.push(std::make_tuple(current_node->right, depth + 1));
 				continue;
 			}
-			if (current_node->point.data[depth%3] < target.data[depth%3]){
-				frontier.push(std::make_tuple(current_node->right, depth+1));
-			} else {
-				frontier.push(std::make_tuple(current_node->left, depth+1));
+			if (current_node->point.data[depth % 3] < target.data[depth % 3])
+			{
+				frontier.push(std::make_tuple(current_node->right, depth + 1));
+			}
+			else
+			{
+				frontier.push(std::make_tuple(current_node->left, depth + 1));
 			}
 			// Check if node in box
-				// If in box, check in circle
-				  // If in circle, add to ids
+			// If in box, check in circle
+			// If in circle, add to ids
 			// Done checks on node, see which children to add
 			// If node on one side of box, add opposite side child
 			// Else add both
 		}
 		return ids;
 	}
-	
-
 };
